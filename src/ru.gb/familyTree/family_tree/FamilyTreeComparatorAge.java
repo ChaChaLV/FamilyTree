775d02aa90cca248d1403c1a96c9ddf80a8397f7
ChaChaLV <ver4ik11@gmail.com>
2024-06-10 23:25:05 +0300

    Add comparator by age

---
+++ src/ru.gb/familyTree/family_tree/FamilyTreeComparatorAge.java
@@ -0,0 +1,13 @@
+package family_tree;
+
+import human.Human;
+
+import java.util.Comparator;
+
+public class FamilyTreeComparatorAge implements Comparator<Human> {
+
+    @Override
+    public int compare(Human o1, Human o2) {
+        return o1.getBirthDate().compareTo(o2.getBirthDate());
+    }
+}
